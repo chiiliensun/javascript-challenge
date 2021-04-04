b13379033795de6ee662b3e531ffa6565fac6c7f
Chii-Lien Sun <chiilien.sun@gmail.com>
2021-04-04 15:39:39 -0700

    started on code, append and forEach functions, missing columns

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
-var tableData = data;
+const tableData = data;
 
-// YOUR CODE HERE!
+// Get reference to the table body
+const tbody = d3.select("tbody");
+
+// console logging the data from data.js
+console.log(data);
+
+// Loop through data
+data.forEach(ufositing => console.log(ufositing));
+
+// Using d3 to append one table row `tr` for each UFO ufoCiting
+data.forEach(ufositing => {
+  let row = tbody.append("tr");
+  let cell = row.append("td");
+  Object.values(uofositing).forEach(value => {
+    let cell = row.append("td");
+    cell.text(value);
+  });
+});
